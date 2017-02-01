0674660440aa2db43f55f404c6f737845a6bf3e2
Patrick Pircher <patrick.pircher@ibm.com>
2017-02-01 16:01:00 +0100

    improvements

--- testem.js
+++
@@ -1,7 +0,0 @@
-var request = require('request');
-request('https://jsonplaceholder.typicode.com/posts/1',{json: true}, function (error, response, body) {
-if (!error && response.statusCode == 200) {
-  console.log(response);
-  console.log(body) // Show the HTML for the Google homepage.
-}
-})
